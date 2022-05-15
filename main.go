106721903e5617f02454be0a66163b5bbd833c20
maxzycon <oskaresvada@yahoo.com>
2022-05-15 15:27:43 +0800

    fix cors origin

--- main.go
+++ main.go
@@ -12,9 +12,26 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	utils.ConnectDB()
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 
 	/**
 	 * init
@@ -25,35 +42,35 @@ func main() {
 	authRoute := routes.AuthRouteInit(auth.InitConstructorController(auth.InitConstructor()))
 
 	// * route group v1 e.g domain.com/api/v1
-	v1 := router.Group("/api/v1")	
-	token,_ := token.NewJWTMaker("12345678901234567890123456789012")
+	v1 := router.Group("/api/v1")
+	token, _ := token.NewJWTMaker("12345678901234567890123456789012")
 	middlewareAuth := v1.Group("/").Use(middleware.AuthMiddleware(token))
 
 	/**
 	 * register login route
 	 * * register login route
 	 */
-	v1.POST("/registration",authRoute.RegistrationUserRoute)
-	v1.POST("/login",authRoute.LoginUserRoute)
+	v1.POST("/registration", authRoute.RegistrationUserRoute)
+	v1.POST("/login", authRoute.LoginUserRoute)
 
 	/**
 	 * category route
-	 * * this is category handler route 
+	 * * this is category handler route
 	 */
-	middlewareAuth.GET("/category",categoryRoutes.GetCategoryRoute)
-	middlewareAuth.GET("/category/:id",categoryRoutes.FindCategoryRoute)
-	middlewareAuth.POST("/category",categoryRoutes.CreateCategoryRoute)
-	middlewareAuth.DELETE("/category/:id",categoryRoutes.DeleteCategoryRoute)
+	v1.GET("/category", categoryRoutes.GetCategoryRoute)
+	// middlewareAuth.GET("/category",categoryRoutes.GetCategoryRoute)
+	middlewareAuth.GET("/category/:id", categoryRoutes.FindCategoryRoute)
+	middlewareAuth.POST("/category", categoryRoutes.CreateCategoryRoute)
+	middlewareAuth.DELETE("/category/:id", categoryRoutes.DeleteCategoryRoute)
 
 	/**
 	 * _todo route
 	 * * this is todo handler route
 	 */
-	middlewareAuth.GET("/todo",todoRoutes.ReadTodo)
-	middlewareAuth.POST("/todo",todoRoutes.CreateTodo)
-	middlewareAuth.PUT("/todo/:id",todoRoutes.UpdateTodo)
-	middlewareAuth.DELETE("/todo/:id",todoRoutes.DeleteTodo)
+	middlewareAuth.GET("/todo", todoRoutes.ReadTodo)
+	middlewareAuth.POST("/todo", todoRoutes.CreateTodo)
+	middlewareAuth.PUT("/todo/:id", todoRoutes.UpdateTodo)
+	middlewareAuth.DELETE("/todo/:id", todoRoutes.DeleteTodo)
 
-	
 	router.Run("0.0.0.0:8080")
 }
